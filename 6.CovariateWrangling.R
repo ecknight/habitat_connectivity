c620ce8743e8fc824125b45707982840450b5512
ecknight <ellyknight@gmail.com>
2021-07-25 09:52:46 -0600

    Add new layers

--- 6.CovariateWrangling.R
+++ 6.CovariateWrangling.R
@@ -16,52 +16,85 @@ band <- read_excel("tbl_band.xlsx") %>%
 
 #2. Wrangle human modification data from GEE----
 hm5 <- read.csv("Covariates/HumanModification_500.csv") %>% 
-  mutate(buffer="500m")
+  dplyr::select(PinpointID, Season, DateTime, Type, Radius, X, Y, mean) %>% 
+  rename(hm.5 = mean)
 hm200 <- read.csv("Covariates/HumanModification_20000.csv") %>% 
-  mutate(buffer="20km")
+  dplyr::select(PinpointID, Season, DateTime, Type, Radius, X, Y, mean) %>% 
+  rename(hm.200 = mean)
 hmpt <- read.csv("Covariates/HumanModification_point.csv") %>% 
-  mutate(buffer="point")
+  dplyr::select(PinpointID, Season, DateTime, Type, Radius, X, Y, mean) %>% 
+  rename(hm.pt = mean)
 
 hm <- hm5 %>% 
-  dplyr::select(PinpointID, Season, DateTime, Type, Radius, X, Y, mean, buffer)
-
-hm <- rbind(hm5, hm200, hmpt) %>% 
-  dplyr::select(PinpointID, Season, DateTime, Type, Radius, Lat, Long, mean, buffer) %>% 
-  rename(hm = mean) %>% 
-  dplyr::filter(Season2 %in% c("Breed1", "Breed2", "Winter", "Winter2")) %>% 
-  arrange(PinpointID, DateTime) %>% 
-  mutate(Season = ifelse(Season2 %in% c("Breed1", "Breed2"), "Breed", "Winter"),
-         ID = row_number(),
-         hm=100*hm)
+  left_join(hm200) %>% 
+  left_join(hmpt)
 
 #4. Wrangle Copernicus landcover from GEE----
-lc200 <- read.csv("Covariates/Landcover_200.csv") %>% 
-  mutate(buffer="200m")
-lc2000 <- read.csv("Covariates/Landcover_2000.csv") %>% 
-  mutate(buffer="2km")
-lc20000 <- read.csv("Covariates/Landcover_20000.csv") %>% 
-  mutate(buffer="20km")
+names <- read.csv("Covariates/Landcover_ClassNames.csv")
 
-lc <- rbind(lc200, lc2000, lc20000) %>% 
-  rename_with(~gsub(pattern=".coverfraction", replacement = "", .x, fixed=TRUE), ends_with(".coverfraction")) %>% 
-  dplyr::select(PinpointID, Season2, DateTime, Lat, Long, bare, crops, grass, moss, shrub, snow, tree, urban, water.permanent, water.seasonal, buffer) %>% 
-  dplyr::filter(Season2 %in% c("Breed1", "Breed2", "Winter", "Winter2")) %>% 
-  arrange(PinpointID, DateTime) %>% 
-  mutate(Season = ifelse(Season2 %in% c("Breed1", "Breed2"), "Breed", "Winter"),
-         ID = row_number()) 
+lc5 <- read.csv("Covariates/Landcover_500.csv") %>% 
+  dplyr::select(-system.index, -ID, -.geo) %>% 
+  rename_with(.cols = c("bare.coverfraction":"water.seasonal.coverfraction"),
+              ~ paste0(., ".5"))
+lc200 <- read.csv("Covariates/Landcover_20000.csv") %>% 
+  dplyr::select(-system.index, -ID, -.geo) %>% 
+  rename_with(.cols = c("bare.coverfraction":"water.seasonal.coverfraction"),
+              ~ paste0(., ".200"))
+lcpt <- read.csv("Covariates/Landcover_point.csv") %>% 
+  dplyr::select(-system.index, -ID, -.geo) %>% 
+  rename(lc = first)
 
-#5. Put all together and add other metadata for individuals----
-covs <- full_join(forest, hm) %>% 
-  full_join(lc) %>% 
-  left_join(band) %>% 
-  mutate(DateTime = ymd_hms(DateTime)) %>% 
-  left_join(dat %>% 
-              dplyr::select(PinpointID, Season2, DateTime, Lat, Long, Winter, Time) %>% 
-              unique() %>% 
-              mutate(DateTime=ymd_hms(DateTime))) %>% 
-  separate(DateTime, into=c("Date", "Time2"), sep=" ") %>% 
-  dplyr::select(-Time2) %>% 
-  mutate(DateTime=ymd_hms(paste0(Date, Time)))
+lc <- lc5 %>% 
+  left_join(lc200) %>% 
+  left_join(lcpt)
+
+#5. Wrangle light pollution from GEE----
+light5 <- read.csv("Covariates/LightPollution_500.csv") %>% 
+  rename(stable.lights.5 = F182013_stable_lights) %>% 
+  dplyr::select(PinpointID, Season, DateTime, Type, Radius, X, Y, stable.lights.5)
+light200 <- read.csv("Covariates/LightPollution_20000.csv") %>% 
+  rename(stable.lights.200 = F182013_stable_lights) %>% 
+  dplyr::select(PinpointID, Season, DateTime, Type, Radius, X, Y, stable.lights.200)
+lightpt <- read.csv("Covariates/LightPollution_point.csv") %>% 
+  rename(stable.lights.pt = F182013_stable_lights) %>% 
+  dplyr::select(PinpointID, Season, DateTime, Type, Radius, X, Y, stable.lights.pt)
+
+light <- light5 %>% 
+  left_join(light200) %>% 
+  left_join(lightpt)
+
+#6. Wrangle drought data from GEE----
+drought5 <- read.csv("Covariates/Drought_500.csv") %>% 
+  dplyr::select(PinpointID, Season, DateTime, Type, Radius, X, Y, mean) %>% 
+  rename(drought.5 = mean)
+drought200 <- read.csv("Covariates/Drought_20000.csv") %>% 
+  dplyr::select(PinpointID, Season, DateTime, Type, Radius, X, Y, mean) %>% 
+  rename(drought.200 = mean)
+droughtpt <- read.csv("Covariates/Drought_point.csv") %>% 
+  dplyr::select(PinpointID, Season, DateTime, Type, Radius, X, Y, mean) %>% 
+  rename(drought.pt = mean)
+
+drought <- drought5 %>% 
+  left_join(drought200) %>% 
+  left_join(droughtpt)
+
+#7. Read in data from other sources----
+pts.covs <- read.csv("ExtractedCovariates.csv") %>% 
+  mutate_all(~ifelse(.=="", NA, .)) %>% 
+  arrange(X, Y, PinpointID, Season, DateTime, Type, Radius) %>% 
+  mutate(ID = row_number()) %>% 
+  dplyr::select(-PinpointID, -Season, -DateTime, -Type, -Radius, -X, -Y)
+
+#8. Put all together and add other metadata for individuals----
+covs <- hm %>% 
+  left_join(lc) %>% 
+  left_join(light) %>% 
+  left_join(drought) %>% 
+  mutate_all(~ifelse(.=="", NA, .)) %>% 
+  arrange(X, Y, PinpointID, Season, DateTime, Type, Radius) %>% 
+  mutate(ID = row_number()) %>% 
+  left_join(pts.covs)
+summary(covs)
   
 write.csv(covs, "Covariates_Breed&Winter.csv", row.names=FALSE)
 
