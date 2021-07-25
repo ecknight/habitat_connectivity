e5b625975fb79b3b3b986f27db4f6bad00a4700a
ecknight <ellyknight@gmail.com>
2021-07-24 19:28:44 -0600

    Update for new vars & pts

--- 6.CovariateWrangling.R
+++ 6.CovariateWrangling.R
@@ -3,14 +3,8 @@ library(readxl)
 library(lubridate)
 library(suncalc)
 
-#OTHER LAYERS THAT I COULD ADD:
-#LIGHT POLLUTION
-#ROADS
-#EVI
-#PESTICIDES?
-
 #1. Read in other datasets----
-dat <- read.csv("/Users/ellyknight/Documents/UoA/Projects/Projects/MCP2/Analysis/Data/CONIMCP_CleanDataAll.csv")
+dat <- read.csv("CONIMCP_CleanDataAll_Habitat_3857.csv")
 
 pop <- read.csv("/Users/ellyknight/Documents/UoA/Projects/Projects/MCP2/Analysis/Data/tbl_population_abundance.csv")
 
@@ -20,35 +14,19 @@ band <- read_excel("tbl_band.xlsx") %>%
   left_join(pop %>% 
               dplyr::select(Population, Abbreviation))
 
-#2. Wrangle forest loss data from GEE----
-forest200 <- read.csv("Covariates/ForestLoss_200.csv") %>% 
-  mutate(buffer="200m")
-forest2000 <- read.csv("Covariates/ForestLoss_2000.csv") %>% 
-  mutate(buffer="2km")
-forest20000 <- read.csv("Covariates/ForestLoss_20000.csv") %>% 
+#2. Wrangle human modification data from GEE----
+hm5 <- read.csv("Covariates/HumanModification_500.csv") %>% 
+  mutate(buffer="500m")
+hm200 <- read.csv("Covariates/HumanModification_20000.csv") %>% 
   mutate(buffer="20km")
+hmpt <- read.csv("Covariates/HumanModification_point.csv") %>% 
+  mutate(buffer="point")
 
-forest <- rbind(forest200, forest2000, forest20000) %>% 
-  dplyr::select(PinpointID, Season2, DateTime, Lat, Long, loss, period, treecover2000, buffer) %>% 
-  mutate(treecover2020 = treecover2000*(1-loss)) %>% 
-  dplyr::filter(Season2 %in% c("Breed1", "Breed2", "Winter", "Winter2")) %>% 
-  arrange(PinpointID, DateTime) %>% 
-  mutate(Season = ifelse(Season2 %in% c("Breed1", "Breed2"), "Breed", "Winter"),
-         ID = row_number(),
-         loss=100*loss,
-         treecover=100*treecover2000,
-         treecover2020=110*treecover2020) 
+hm <- hm5 %>% 
+  dplyr::select(PinpointID, Season, DateTime, Type, Radius, X, Y, mean, buffer)
 
-#3. Wrangle human modification data from GEE----
-hm200 <- read.csv("Covariates/HumanModification_200.csv") %>% 
-  mutate(buffer="200m")
-hm2000 <- read.csv("Covariates/HumanModification_2000.csv") %>% 
-  mutate(buffer="2km")
-hm20000 <- read.csv("Covariates/HumanModification_20000.csv") %>% 
-  mutate(buffer="20km")
-
-hm <- rbind(hm200, hm2000, hm20000) %>% 
-  dplyr::select(PinpointID, Season2, DateTime, Lat, Long, mean, buffer) %>% 
+hm <- rbind(hm5, hm200, hmpt) %>% 
+  dplyr::select(PinpointID, Season, DateTime, Type, Radius, Lat, Long, mean, buffer) %>% 
   rename(hm = mean) %>% 
   dplyr::filter(Season2 %in% c("Breed1", "Breed2", "Winter", "Winter2")) %>% 
   arrange(PinpointID, DateTime) %>% 
