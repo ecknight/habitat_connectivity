e48c1aecf19f584a0891a9cbc9b324ac3c298618
ecknight <ellyknight@gmail.com>
2021-07-25 09:52:33 -0600

    Finish pesticide extraction

--- 5.CovariateExtraction.R
+++ 5.CovariateExtraction.R
@@ -26,24 +26,24 @@ rds.200 <- rds %>%
   st_intersection(pts.200)
 
 #5. Calculate length per buffer----
-length.5 <- data.frame(Length5 = st_length(rds.5)) %>% 
+length.5 <- data.frame(Length.5 = st_length(rds.5)) %>% 
   cbind(rds.5) %>% 
   data.frame() %>% 
   group_by(ID) %>% 
-  summarize(Length5 = sum(Length5))
+  summarize(Length.5 = sum(Length.5))
 
-length.200 <- data.frame(Length200 = st_length(rds.200)) %>% 
+length.200 <- data.frame(Length.200 = st_length(rds.200)) %>% 
   cbind(rds.200) %>% 
   data.frame() %>% 
   group_by(ID) %>% 
-  summarize(Length200 = sum(Length200))
+  summarize(Length.200 = sum(Length.200))
   
 #6. Put back together----
 pts.rds <- pts.all.sf %>% 
   left_join(length.5) %>% 
   left_join(length.200) %>% 
-  mutate(Length5 =ifelse(is.na(Length5), 0, Length5),
-         Length200 = ifelse(is.na(Length200), 0, Length200))
+  mutate(Length.5 =ifelse(is.na(Length.5), 0, Length.5),
+         Length.200 = ifelse(is.na(Length.200), 0, Length.200))
 
 #B. PESTICIDES####
 #7. Get list of tifs----
@@ -61,15 +61,31 @@ tifs.use.list <- as.list(tifs.use)
 
 #9. Stack all tifs & calculate mean----
 stack.all <- raster::stack(tifs.use.list[["filepath"]])
-stack.all.scale <- scale(stack.all)
-pesticides.mean <- calc(stack.all, fun=mean)
+#stack.all.scale <- scale(stack.all)
+#pesticides.mean <- calc(stack.all.scale, fun=mean)
 
-writeRaster(pesticides.mean, "Shapefiles/PesticidesMean.tif")
-pesticides.mean <- raster("Shapefiles/PesticidesMean.tif")
+#writeRaster(pesticides.mean, "Shapefiles/PesticidesMean.tif", overwrite=TRUE)
+pesticides.mean <- raster("Shapefiles/PesticidesMean.tif") %>% 
+  projectRaster(crs=3857)
+plot(pesticides.mean)
 
 #10. Extract raster values----
-pest.200 <- raster::extract(pesticides.mean, pts.200, fun=mean, df=TRUE)
-pest.5 <- raster::extract(pesticides.mean, pts.5, fun=mean, df=TRUE)
-pest.pt <- raster::extract(pesticides.mean, pts, df=TRUE)
+pest.200 <- raster::extract(pesticides.mean, pts.200, fun=mean, df=TRUE) %>% 
+  rename(pest.200 = PesticidesMean) 
+pest.5 <- raster::extract(pesticides.mean, pts.5, fun=mean, df=TRUE) %>% 
+  rename(pest.5 = PesticidesMean)
+pest.pt <- raster::extract(pesticides.mean, pts.all.sf, df=TRUE) %>% 
+  rename(pest.pt = PesticidesMean)
 
 #11. Put back together----
+pts.pest <- cbind(pts.all.sf, pest.200, pest.5, pest.pt) %>% 
+  data.frame() %>% 
+  dplyr::select(-geometry, -ID.1, -ID.2, -ID.3)
+
+#12. Put all together and save----
+pts.covs <- pts.rds %>% 
+  left_join(pts.pest) %>% 
+  data.frame() %>% 
+  dplyr::select(-ID, -geometry)
+
+write.csv(pts.covs, "ExtractedCovariates.csv", row.names = FALSE)
