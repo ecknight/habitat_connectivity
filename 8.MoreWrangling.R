914b160d7a8d16f9ce6f6aad1c18adb011ec0e66
ecknight <ellyknight@gmail.com>
2021-07-25 19:52:04 -0600

    Initial commit

---
+++ 8.MoreWrangling.R
@@ -0,0 +1,60 @@
+library(tidyverse)
+
+options(scipen = 999)
+
+#1. Read in metadata----
+dat.hab <- read.csv("CONIMCP_CleanDataAll_Habitat.csv") %>% 
+  st_as_sf(coords=c("Long", "Lat"), crs=4326) %>% 
+  st_transform(crs=3857) %>% 
+  st_coordinates() %>% 
+  cbind(read.csv("CONIMCP_CleanDataAll_Habitat.csv")) %>% 
+  dplyr::filter(Winter==1) %>% 
+  mutate(GPS = ifelse(Type=="Band", "Band", "GPS")) %>% 
+  dplyr::select(PinpointID, DateTime, SeasonR2n, X, Y, GPS, Population, Mass, Wing, Sex, Year, doy, sun)
+
+#2. ID birds that need the banding point for used
+dat.hab.breed <- dat.hab %>% 
+  dplyr::filter(SeasonR2n %in% c("Breed1", "Breed2"))
+
+dat.hab.band <- data.frame(table(dat.hab.breed$GPS, dat.hab.breed$PinpointID)) %>% 
+  rename(GPS = Var1, PinpointID=Var2) %>% 
+  dplyr::filter(Freq==0, GPS=="GPS") %>% 
+  dplyr::select(PinpointID) %>% 
+  mutate(BreedPt=1,
+         SeasonR2n="Breed1",
+         PinpointID=as.integer(as.character(PinpointID)))
+
+#filter out non-roosting points here
+dat.hab.use <- dat.hab %>% 
+  left_join(dat.hab.band) %>% 
+  mutate(X=round(X), Y=round(Y)) %>% 
+  dplyr::filter(BreedPt==1 | sun==1) %>% 
+  rename(Season=SeasonR2n) %>% 
+  arrange(PinpointID, DateTime, X, Y)
+
+table(dat.hab.use$GPS, dat.hab.use$PinpointID, dat.hab.use$Season)
+table(dat.hab.use$sun)
+
+#3. Read in covariates & wrangle----
+#Remove NAs for modelling (all available pts)
+#Remove banding points for most birds
+#Filter by roosting pts
+#Add population
+
+pop <- dat.hab.use %>% 
+  dplyr::select(PinpointID, Population) %>% 
+  unique() %>% 
+  rename(Population2 = Population)
+
+covs <- read.csv("Covariates_Breed&Winter_VIF.csv") %>%   
+  dplyr::filter(!is.na(drought.pt), !is.na(hm.pt)) %>% 
+  mutate(X=round(X), Y=round(Y)) %>% 
+  left_join(dat.hab.use %>% 
+              dplyr::select(-Season)) %>% 
+  arrange(PinpointID, DateTime, X, Y) %>% 
+  dplyr::filter(!(Type=="Used" & is.na(Population))) %>% 
+  left_join(pop) %>% 
+  mutate(Population = ifelse(is.na(Population), Population2, Population)) %>% 
+  dplyr::select(-Population2)
+
+write.csv(covs, "Covariates_Breed&Winter_Metadata.csv", row.names = FALSE)
